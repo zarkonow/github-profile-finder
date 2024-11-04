c3a59675795cc078ea23a7599b8ad93b422d48c0
zarkonow <stojanov1987@gmail.com>
2024-11-04 22:52:28 +0100

    *added input

---
+++ src/github-profile-finder/index.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+export default function GithubProfileFinder() {
+  const [userName, setUserName] = useState("zarkonow");
+
+
+async function fetchGithHubUserData() {
+    const res = await fetch(`https://api.github.com/users/${userName}`)
+
+    const data = res.json();
+    console.log(data);
+    
+}
+
+
+  function handleSubmit() {
+
+  }
+
+
+  useEffect(() => {
+   fetchGithHubUserData()
+  }, [])
+  
+
+  return (
+    <div className="github-profile-container">
+      <div className="input-wrapper">
+        <input
+          name="search-by-username"
+          type="text"
+          placeholder="Search Github username"
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
+    </div>
+  );
+}
